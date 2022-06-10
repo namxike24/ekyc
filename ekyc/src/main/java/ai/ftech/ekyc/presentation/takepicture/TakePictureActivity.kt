d137830c4741733f267ad98bd457f048f3e88c7d
anhnd <anhnduy@ftech.ai>
2022-06-10 17:08:50 +0700

    add code

--- ekyc/src/main/java/ai/ftech/ekyc/presentation/takepicture/TakePictureActivity.kt
+++ ekyc/src/main/java/ai/ftech/ekyc/presentation/takepicture/TakePictureActivity.kt
@@ -1,5 +1,6 @@
 package ai.ftech.ekyc.presentation.takepicture
 
+import ai.ftech.dev.base.extension.getAppDrawable
 import ai.ftech.dev.base.extension.setOnSafeClick
 import ai.ftech.ekyc.R
 import ai.ftech.ekyc.common.FEkycActivity
@@ -10,6 +11,7 @@ import com.otaliastudios.cameraview.CameraListener
 import com.otaliastudios.cameraview.CameraView
 import com.otaliastudios.cameraview.PictureResult
 import com.otaliastudios.cameraview.controls.Facing
+import com.otaliastudios.cameraview.controls.Flash
 
 class TakePictureActivity : FEkycActivity(R.layout.fekyc_take_picture_activity) {
     private lateinit var cvCameraView: CameraView
@@ -62,7 +64,15 @@ class TakePictureActivity : FEkycActivity(R.layout.fekyc_take_picture_activity) 
         })
 
         ivFlash.setOnSafeClick {
-
+            if (viewModel.isFlash) {
+                cvCameraView.flash = Flash.OFF
+                ivFlash.setImageDrawable(getAppDrawable(R.drawable.fekyc_ic_flash_off))
+                viewModel.isFlash = false
+            } else {
+                cvCameraView.flash = Flash.ON
+                ivFlash.setImageDrawable(getAppDrawable(R.drawable.fekyc_ic_flash_on))
+                viewModel.isFlash = true
+            }
         }
 
         ivCapture.setOnSafeClick {
--- ekyc/src/main/java/ai/ftech/ekyc/presentation/takepicture/TakePictureViewModel.kt
+++ ekyc/src/main/java/ai/ftech/ekyc/presentation/takepicture/TakePictureViewModel.kt
@@ -3,6 +3,6 @@ package ai.ftech.ekyc.presentation.takepicture
 import ai.ftech.dev.base.common.BaseViewModel
 
 class TakePictureViewModel : BaseViewModel() {
-
     var isFrontFace = false
+    var isFlash = false
 }
