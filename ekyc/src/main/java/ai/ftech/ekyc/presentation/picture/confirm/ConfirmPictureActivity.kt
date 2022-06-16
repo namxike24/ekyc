57ae0f35631d703d5f21ab4c5e1dced65925805b
anhnd <anhnduy@ftech.ai>
2022-06-16 13:43:57 +0700

    add confirm picture screen

---
+++ ekyc/src/main/java/ai/ftech/ekyc/presentation/picture/confirm/ConfirmPictureActivity.kt
@@ -0,0 +1,13 @@
+package ai.ftech.ekyc.presentation.picture.confirm
+
+import ai.ftech.ekyc.R
+import ai.ftech.ekyc.common.FEkycActivity
+
+class ConfirmPictureActivity : FEkycActivity(R.layout.fekyc_confirm_picture_activity) {
+
+
+
+    override fun onInitView() {
+        super.onInitView()
+    }
+}
