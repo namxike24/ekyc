bbaf14e344bdfb6971ae67e70bb9bac0c5ac77f9
anhnd <anhnduy@ftech.ai>
2022-06-17 17:37:33 +0700

    add coding

--- ekyc/src/main/java/ai/ftech/ekyc/data/repo/InfoRepoImpl.kt
+++ ekyc/src/main/java/ai/ftech/ekyc/data/repo/InfoRepoImpl.kt
@@ -1,12 +1,13 @@
 package ai.ftech.ekyc.data.repo
 
 import ai.ftech.dev.base.repo.BaseRepo
+import ai.ftech.ekyc.domain.model.UserInfo
 import ai.ftech.ekyc.domain.model.address.City
 import ai.ftech.ekyc.domain.model.address.Nation
 import ai.ftech.ekyc.domain.repo.IInfoRepo
 
 class InfoRepoImpl : BaseRepo(), IInfoRepo {
-    override fun getUserInfo() {
+    override fun getUserInfo(): List<UserInfo> {
         TODO("Not yet implemented")
     }
 
--- ekyc/src/main/java/ai/ftech/ekyc/domain/repo/IInfoRepo.kt
+++ ekyc/src/main/java/ai/ftech/ekyc/domain/repo/IInfoRepo.kt
@@ -1,10 +1,11 @@
 package ai.ftech.ekyc.domain.repo
 
+import ai.ftech.ekyc.domain.model.UserInfo
 import ai.ftech.ekyc.domain.model.address.City
 import ai.ftech.ekyc.domain.model.address.Nation
 
 interface IInfoRepo {
-    fun getUserInfo()
+    fun getUserInfo() : List<UserInfo>
     fun getCityList(): List<City>
     fun getNationList(): List<Nation>
 }
